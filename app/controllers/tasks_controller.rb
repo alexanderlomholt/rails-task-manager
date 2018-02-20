6ba5ee04670271ee77dd59493fbe26c1c491ba9e
Alexander Lomholt <arlomholt@gmail.com>
2018-02-20 16:08:09 -0500

    added verification before items are deleted

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,10 +25,11 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     @task.update(task_params)
-    redirect_to tasks_path
+    redirect_to task_path
   end
 
   def destroy
+
     @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
